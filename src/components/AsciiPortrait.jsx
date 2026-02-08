b19a5b2941a60a2156e7891cccf85bc15a955017
Niranjan7771 <yalamniranjan@gmail.com>
2026-02-08 14:21:50 +0530

    Move ascii portrait to public folder

--- src/components/AsciiPortrait.jsx
+++ src/components/AsciiPortrait.jsx
@@ -1,19 +1,18 @@
 
 import React from 'react';
-import asciiProfile from '../assets/ascii-profile.png';
 
 const AsciiPortrait = () => {
     return (
         <div style={{ width: '100%', height: '100%', overflow: 'hidden', background: '#000', borderRadius: 'inherit' }}>
             <img
-                src={asciiProfile}
+                src="/ascii-profile.png"
                 alt="ASCII Portrait"
                 style={{
                     display: 'block',
                     width: '100%',
                     height: '100%',
                     objectFit: 'contain',
-                    imageRendering: 'pixelated' // Keep it sharp
+                    imageRendering: 'pixelated'
                 }}
             />
         </div>
